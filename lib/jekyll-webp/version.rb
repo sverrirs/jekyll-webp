e436d8828c9ac173fdbb843cbe222bb0139fefa5
Emilio Del Tessandoro <emilio@spotify.com>
2017-11-20 16:11:03 +0100

    recursively select images and don't write to source directory

--- lib/jekyll-webp/version.rb
+++ lib/jekyll-webp/version.rb
@@ -1,6 +1,6 @@
 module Jekyll
   module Webp
-    VERSION = "0.1.0"
+    VERSION = "0.1.1"
     # When modifying remember to issue a new tag command in git before committing, then push the new tag
     # git tag -a v0.1.0 -m "Gem v0.1.0"
     # git push origin --tags
